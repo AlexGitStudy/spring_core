a47ff8d5b692db8f4a56b1802217b1517e27055a
Alexander Mironov <mironale@fel.cvut.cz>
2017-06-07 18:33:49 +0200

    11 deleni xml a import xml

--- src/main/java/com/github/alexgitstudy/project/robot/main/Start.java
+++ src/main/java/com/github/alexgitstudy/project/robot/main/Start.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Object object = context.getBean("t1000Empty");
+        Object object = context.getBean("t1000");
         if (object instanceof ModelT1000) {
             ModelT1000 t1000 = (ModelT1000) object;
 //            t1000.action();
